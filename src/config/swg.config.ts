6a222f8e7399647c761bd4c3c4a1c11644538955
lizhilizhi <1163645478@qq.com>
2022-07-19 14:45:12 +0800

    增加config 把swagger的部分配置拆解

---
+++ src/config/swg.config.ts
@@ -0,0 +1,6 @@
+import { DocumentBuilder } from '@nestjs/swagger';
+
+const options  = new DocumentBuilder().setTitle('wuwuqianqian')
+  .setDescription('wuwuqiaqian').build();
+
+export {options};
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-
+import { options } from './config/swg.config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const options = new DocumentBuilder().setTitle('').build();
-  const document = SwaggerModule.createDocument(app, options);
+  const document = SwaggerModule.createDocument(app,options);
   SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
