ce39ca4d1d3f562db3575424980b25fa364bf9fd
lizhilizhi <1163645478@qq.com>
2022-08-25 21:05:01 +0800

    增加 任务调度

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,17 +1,13 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import {Cron} from "@nestjs/schedule";
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+
 
 
   @Cron('45 * * * * *')
-  hacron() {
+  expCron() {
     console.log('---执行一次----')
   }
 }
