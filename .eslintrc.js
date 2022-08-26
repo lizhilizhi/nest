a6bf94f57adf16e520bccf2236e359c615ee7b8a
lizhilizhi <1163645478@qq.com>
2022-08-26 13:39:12 +0800

    增加数据库配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
+    project: 'ormconfig.json',
     tsconfigRootDir : __dirname, 
     sourceType: 'module',
   },
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import {ScheduleModule} from "@nestjs/schedule";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {DataSource} from "typeorm";
 
 @Module({
-  imports: [UserModule, ScheduleModule.forRoot()],
+  imports: [UserModule, ScheduleModule.forRoot(),TypeOrmModule.forRoot(
+      {
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: 'lizhi1234',
+        database: 'text',
+        entities: ['dist/**/*.entity{ .ts,.js}'],
+        synchronize: true,}
+  )],
   controllers: [AppController],
   providers: [AppService,],
 })
-export class AppModule  {}
+export class AppModule  {
+  constructor(private dataSource: DataSource) {}
+}
